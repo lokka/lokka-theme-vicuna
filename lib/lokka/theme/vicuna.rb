7ccce0c480efba48408902496a98c7f92985cd79
Masaki Komagata <komagata@gmail.com>
2014-05-06 00:33:44 +0900

    Fix

--- lib/lokka/theme/vicuna.rb
+++ lib/lokka/theme/vicuna.rb
@@ -5,12 +5,14 @@ module Lokka
   module Theme
     module Vicuna
       def self.registered(app)
-        path = File.expand_path('../../../views', __FILE__)
-        puts path
-        puts app.themes
+        path = File.expand_path('../../../../views', __FILE__)
+        app.set :views, path
+        puts "path: #{path}"
+        puts "app.settings.views: #{app.settings.views}"
+        puts "app.settings: #{app.settings}"
       end
     end
 
-    register Vicuna
+    Sinatra.register Vicuna
   end
 end

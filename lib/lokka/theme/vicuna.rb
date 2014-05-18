009a769d7debe5075ee8f07e88342a0f4a2cb980
Masaki Komagata <komagata@gmail.com>
2014-05-18 23:36:02 +0900

    Update bundler

--- lib/lokka/theme/vicuna.rb
+++ lib/lokka/theme/vicuna.rb
@@ -5,14 +5,14 @@ module Lokka
   module Theme
     module Vicuna
       def self.registered(app)
+        puts 'vicuna!!!'
         path = File.expand_path('../../../../views', __FILE__)
         app.set :views, path
+        app.set :themes, app.settings.themes.merge(vicuna: path)
         puts "path: #{path}"
         puts "app.settings.views: #{app.settings.views}"
         puts "app.settings: #{app.settings}"
       end
     end
-
-    Sinatra.register Vicuna
   end
 end

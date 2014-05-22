6a9da860641e7ef0598e5284359826b97b8d7c67
Masaki Komagata <komagata@gmail.com>
2014-05-23 01:47:09 +0900

    Fix

--- lib/lokka-theme-vicuna.rb
+++ lib/lokka-theme-vicuna.rb
@@ -1,14 +1,14 @@
-require 'lokka/theme/vicuna/version'
 require 'sinatra/base'
 
 module Lokka
   module Theme
     module Vicuna
+      VERSION = '0.0.2'
+
       def self.registered(app)
         puts 'vicuna!!!'
         path = File.expand_path('../../../../views', __FILE__)
         app.set :views, path
-        app.set :themes, app.settings.themes.merge(vicuna: path)
         puts "path: #{path}"
         puts "app.settings.views: #{app.settings.views}"
         puts "app.settings: #{app.settings}"
--- lib/lokka/theme/vicuna/version.rb
+++
@@ -1,7 +0,0 @@
-module Lokka
-  module Theme
-    module Vicuna
-      VERSION = "0.0.1"
-    end
-  end
-end
